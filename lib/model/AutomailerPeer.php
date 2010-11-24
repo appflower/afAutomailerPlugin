5a8d8e4d15d62b86f6089316e4e8ea98061b1078
Radu Topala <radu.topala@trisoft.ro>
2010-11-24 14:54:09 +0000

    modified afAutomailer

--- lib/model/AutomailerPeer.php
+++ lib/model/AutomailerPeer.php
@@ -4,7 +4,7 @@ class AutomailerPeer extends BaseAutomailerPeer
 {
     public static function getEmailsForSending() {
         $c = new Criteria();
-        $c->addAscendingOrderByColumn(Automailer::ID);
+        $c->addAscendingOrderByColumn(self::ID);
         $c->add(self::IS_SENT, 0);
         $c->addAnd(self::IS_FAILED, 0);
         $c->setLimit(sfConfig::get('app_afAutomailerPlugin_emails_limit'));
